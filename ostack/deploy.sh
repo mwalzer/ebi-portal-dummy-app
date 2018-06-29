bf2a0307711c8070c6cef15f4f1079f91a40660d
walzer <walzer@ebi.ac.uk>
2018-06-29 13:55:33 +0100

    fine tuning deployment structure pt14

--- ostack/deploy.sh
+++ ostack/deploy.sh
@@ -59,7 +59,8 @@ terraform apply --state=$PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'
 echo "＼(＾O＾)／ Prepare the deployment substructure and link infrastructure"
 mkdir -p $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/kubespray'
 cp $PORTAL_APP_REPO_FOLDER'/kubespray-2.3.0/contrib/terraform/terraform.py' $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/terraform.py'
-ln -s $PORTAL_APP_REPO_FOLDER'/kubespray/inventory/group_vars' $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/'
+cp -r $PORTAL_APP_REPO_FOLDER'/kubespray-2.3.0/inventory/group_vars' $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/'
+cat $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/group_vars/no-floating.yml'
 
 echo "＼(＾O＾)／ symlink the playbooks, the inventory files"
 #for i in $PORTAL_APP_REPO_FOLDER'/kubespray/*'; do
