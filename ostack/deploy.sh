732f7f02311e747fb7d6fa965c5a5d5fb8a2e730
walzer <walzer@ebi.ac.uk>
2018-08-14 13:34:52 +0100

    added deploy retries
    because somehow sometimes our cloud does not provide ssh connection the first time (in deploy container a second or third try succeeds)

--- ostack/deploy.sh
+++ ostack/deploy.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-
-# Exit immediately if a command exits with a non-zero status
-# (but allow for the error trap)
-set -eE
+set -euo pipefail
 
 # keys exists at $PUBLIC_KEY, $PRIVATE_KEY and profile key at $ssh_key
 export TF_VAR_public_key_path=$PUBLIC_KEY
@@ -83,48 +80,106 @@ ansible-playbook --version
 echo "＼(＾O＾)／ Giving cloudinit some more time (and avoid ssh unavailability)"
 sleep 10
 
+maxRetries=3
+retryInterval=10
+
 echo "＼(＾O＾)／ Applying ansible playbooks"
 cd $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/kubespray'
 echo "cwd=$PWD"
+
 # Provision kubespray
-ansible-playbook --flush-cache -b --become-user=root -i $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/deployment/terraform.py' cluster.yml \
-	--key-file "$PRIVATE_KEY" \
-	-e bootstrap_os=ubuntu \
-	-e host_key_checking=false \
-	-e cloud_provider="openstack" \
-	-e efk_enabled=false \
-	-e kubelet_deployment_type=$KUBELET_DEPLOYMENT_TYPE \
-	-e kube_api_pwd=$TF_VAR_kube_api_pwd \
-	-e cluster_name=$TF_VAR_cluster_name \
-	-e helm_enabled=true \
-	-e kube_version=$KUBE_VERSION \
-	-e kube_network_plugin="weave"
+retry=0
+until [ ${retry} -ge ${maxRetries} ]
+do
+  ansible-playbook --flush-cache -b --become-user=root -i $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/deployment/terraform.py' cluster.yml \
+  	--key-file "$PRIVATE_KEY" \
+  	-e bootstrap_os=ubuntu \
+  	-e host_key_checking=false \
+  	-e cloud_provider="openstack" \
+  	-e efk_enabled=false \
+  	-e kubelet_deployment_type=$KUBELET_DEPLOYMENT_TYPE \
+  	-e kube_api_pwd=$TF_VAR_kube_api_pwd \
+  	-e cluster_name=$TF_VAR_cluster_name \
+  	-e helm_enabled=true \
+  	-e kube_version=$KUBE_VERSION \
+  	-e kube_network_plugin="weave" && break
+	retry=$[${retry}+1]
+	echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
+	sleep ${retryInterval}
+done
+if [ ${retry} -ge ${maxRetries} ]; then
+  echo "Failed after ${maxRetries} attempts!"
+  exit 1
+fi
 
 # Provision glusterfs
-ansible-playbook -b --become-user=root -i $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/deployment/terraform.py' contrib/network-storage/glusterfs/glusterfs.yml \
-	--key-file "$PRIVATE_KEY" \
-	-e host_key_checking=false \
-	-e bootstrap_os=ubuntu
+until [ ${retry} -ge ${maxRetries} ]
+do
+  ansible-playbook -b --become-user=root -i $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/deployment/terraform.py' contrib/network-storage/glusterfs/glusterfs.yml \
+  	--key-file "$PRIVATE_KEY" \
+  	-e host_key_checking=false \
+  	-e bootstrap_os=ubuntu && break
+	retry=$[${retry}+1]
+	echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
+	sleep ${retryInterval}
+done
+if [ ${retry} -ge ${maxRetries} ]; then
+  echo "Failed after ${maxRetries} attempts!"
+  exit 1
+fi
 
 # Set permissive access control and add '30700 open' security group
-ansible-playbook -b --become-user=root \
-  -i $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/deployment/terraform.py' \
-	$PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/extra_playbooks/rbac/rbac.yml' \
-	--key-file "$PRIVATE_KEY"
+until [ ${retry} -ge ${maxRetries} ]
+do
+  ansible-playbook -b --become-user=root \
+    -i $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/deployment/terraform.py' \
+  	$PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/extra_playbooks/rbac/rbac.yml' \
+  	--key-file "$PRIVATE_KEY" && break
+	retry=$[${retry}+1]
+	echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
+	sleep ${retryInterval}
+done
+if [ ${retry} -ge ${maxRetries} ]; then
+  echo "Failed after ${maxRetries} attempts!"
+  exit 1
+fi
 
 # Start Galaxy, provision galaxy dataset, start workflow
-ansible-playbook -b --become-user=root \
-  -i $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/deployment/terraform.py' \
-	$PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/extra_playbooks/k8s-galaxy/k8s-galaxy.yml' \
-	--key-file "$PRIVATE_KEY"
+until [ ${retry} -ge ${maxRetries} ]
+do
+  ansible-playbook -b --become-user=root \
+    -i $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/deployment/terraform.py' \
+  	$PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/extra_playbooks/k8s-galaxy/k8s-galaxy.yml' \
+  	--key-file "$PRIVATE_KEY" && break
+	retry=$[${retry}+1]
+	echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
+	sleep ${retryInterval}
+done
 #  --write_to_/opt/galaxy_data/test.txt
+if [ ${retry} -ge ${maxRetries} ]; then
+  echo "Failed after ${maxRetries} attempts!"
+  exit 1
+fi
+
 
 # wait for write_to_/opt/galaxy_data/test.txt and write to local file
-ansible-playbook -b --become-user=root -i $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/deployment/terraform.py' \
-	$PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/extra_playbooks/get-results/get-results.yml' \
-	--key-file "$PRIVATE_KEY" \
-  --extra-vars "helm_test_param=789"
-  #something like --extra-vars "helm_test_param=helm_test_param_in"
+until [ ${retry} -ge ${maxRetries} ]
+do
+  ansible-playbook -b --become-user=root -i $PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/deployment/terraform.py' \
+  	$PORTAL_DEPLOYMENTS_ROOT'/'$PORTAL_DEPLOYMENT_REFERENCE'/extra_playbooks/get-results/get-results.yml' \
+  	--key-file "$PRIVATE_KEY" \
+
+    --extra-vars "helm_test_param=789" && break
+	#something like --extra-vars "helm_test_param=helm_test_param_in"
+  retry=$[${retry}+1]
+	echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
+	sleep ${retryInterval}
+done
+if [ ${retry} -ge ${maxRetries} ]; then
+  echo "Failed after ${maxRetries} attempts!"
+  exit 1
+fi
+
 
 
 helm_test_param_out=`cat helm_test_param.txt`
