86b25fdb371c5cb3c932bc36c2f585e40bb07783
walzer <walzer@ebi.ac.uk>
2018-09-04 16:31:16 +0100

    retracing Giannis fixes and clean ups

--- ostack/deploy.sh
+++ ostack/deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-set -euoE pipefail
+set -euoEx pipefail
 
 echo "＼(＾O＾)／ Setting up convenience OS_ENV"
 # Git cloned application folder
@@ -43,14 +43,15 @@ terraform apply --state=$TF_STATE $KUBESPRAY_TF
 #python terraform.py --list --root .
 
 echo "＼(＾O＾)／ Giving cloudinit some more time (and avoid ssh unavailability)"
-sleep 12
+sleep 30
 
+pkill -f 'ssh-agent -s'
 #Seems the agent is critical for forwarding the PRIV_KEY_PATH to the endpoint host
-#eval $(ssh-agent -s)
-#ssh-add $PRIV_KEY_PATH
+eval $(ssh-agent -s)
+ssh-add $PRIV_KEY_PATH
 
 maxRetries=3
-retryInterval=10
+retryInterval=60
 
 echo "＼(＾O＾)／ Applying ansible playbooks"
 cd $DPL'/kubespray'
@@ -63,20 +64,20 @@ until [ ${retry} -ge ${maxRetries} ]
 do
   ansible-playbook --flush-cache -b --become-user=root -i $DPL'/kubespray/inventory/terraform.py' \
     $DPL'/kubespray/cluster.yml' \
-  	--key-file "$PRIV_KEY_PATH" \
-  	-e bootstrap_os=ubuntu \
-  	-e host_key_checking=false \
-  	-e cloud_provider="openstack" \
-  	-e efk_enabled=false \
-  	-e kubelet_deployment_type=$KUBELET_DEPLOYMENT_TYPE \
-  	-e kube_api_pwd=$TF_VAR_kube_api_pwd \
-  	-e cluster_name=$TF_VAR_cluster_name \
-  	-e helm_enabled=true \
-  	-e kube_version=$KUBE_VERSION \
-  	-e kube_network_plugin="weave" && break
-	retry=$[${retry}+1]
-	echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
-	sleep ${retryInterval}
+    --key-file "$PRIV_KEY_PATH" \
+    -e bootstrap_os=ubuntu \
+    -e host_key_checking=false \
+    -e cloud_provider="openstack" \
+    -e efk_enabled=false \
+    -e kubelet_deployment_type=$KUBELET_DEPLOYMENT_TYPE \
+    -e kube_api_pwd=$TF_VAR_kube_api_pwd \
+    -e cluster_name=$TF_VAR_cluster_name \
+    -e helm_enabled=true \
+    -e kube_version=$KUBE_VERSION \
+    -e kube_network_plugin="weave" && break
+    retry=$[${retry}+1]
+    echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
+    sleep ${retryInterval}
 done
 if [ ${retry} -ge ${maxRetries} ]; then
   echo "Failed after ${maxRetries} attempts!"
@@ -89,12 +90,12 @@ do
   ansible-playbook -b --become-user=root \
     -i $DPL'/kubespray/inventory/terraform.py' \
     $DPL'/kubespray/contrib/network-storage/glusterfs/glusterfs.yml' \
-  	--key-file "$PRIV_KEY_PATH" \
-  	-e host_key_checking=false \
-  	-e bootstrap_os=ubuntu && break
-	retry=$[${retry}+1]
-	echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
-	sleep ${retryInterval}
+    --key-file "$PRIV_KEY_PATH" \
+    -e host_key_checking=false \
+    -e bootstrap_os=ubuntu && break
+    retry=$[${retry}+1]
+    echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
+    sleep ${retryInterval}
 done
 if [ ${retry} -ge ${maxRetries} ]; then
   echo "Failed after ${maxRetries} attempts!"
@@ -106,11 +107,11 @@ until [ ${retry} -ge ${maxRetries} ]
 do
   ansible-playbook -b --become-user=root \
     -i $DPL'/kubespray/inventory/terraform.py' \
-  	$DPL'/extra-playbooks/rbac/rbac.yml' \
-  	--key-file "$PRIV_KEY_PATH" && break
-	retry=$[${retry}+1]
-	echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
-	sleep ${retryInterval}
+    $DPL'/extra-playbooks/rbac/rbac.yml' \
+    --key-file "$PRIV_KEY_PATH" && break
+    retry=$[${retry}+1]
+    echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
+    sleep ${retryInterval}
 done
 if [ ${retry} -ge ${maxRetries} ]; then
   echo "Failed after ${maxRetries} attempts!"
@@ -122,11 +123,11 @@ until [ ${retry} -ge ${maxRetries} ]
 do
   ansible-playbook -b --become-user=root \
     -i $DPL'/kubespray/inventory/terraform.py' \
-  	$DPL'/extra-playbooks/k8s-galaxy/k8s-galaxy.yml' \
-  	--key-file "$PRIV_KEY_PATH" && break
-	retry=$[${retry}+1]
-	echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
-	sleep ${retryInterval}
+    $DPL'/extra-playbooks/k8s-galaxy/k8s-galaxy.yml' \
+    --key-file "$PRIV_KEY_PATH" && break
+    retry=$[${retry}+1]
+    echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
+    sleep ${retryInterval}
 done
 #  --write_to_/opt/galaxy_data/test.txt
 if [ ${retry} -ge ${maxRetries} ]; then
@@ -140,12 +141,12 @@ until [ ${retry} -ge ${maxRetries} ]
 do
   ansible-playbook -b --become-user=root \
     -i $DPL'/kubespray/inventory/terraform.py' \
-  	$DPL'/extra-playbooks/wf-controller/wf-controller.yml' \
-  	--key-file "$PRIV_KEY_PATH" && break
-	#something like --extra-vars "helm_test_param=helm_test_param_in"
+    $DPL'/extra-playbooks/wf-controller/wf-controller.yml' \
+    --key-file "$PRIV_KEY_PATH" && break
+    #something like --extra-vars "helm_test_param=helm_test_param_in"
   retry=$[${retry}+1]
-	echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
-	sleep ${retryInterval}
+    echo "Retrying [${retry}/${maxRetries}] in ${retryInterval}(s) "
+    sleep ${retryInterval}
 done
 if [ ${retry} -ge ${maxRetries} ]; then
   echo "Failed after ${maxRetries} attempts!"
